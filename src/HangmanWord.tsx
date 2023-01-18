3f5656d3a462d8bc45f2230636d503df17f53347
Adam Lang <adam.lang-1@hotmail.com>
2023-01-18 21:36:34 +0200

    feat: create span for individual letter

--- src/HangmanWord.tsx
+++ src/HangmanWord.tsx
@@ -1,3 +1,14 @@
+import '../src/styles/hangmanword.css';
+
 export function HangmanWord() {
-    
+    const word = 'test';
+    return (
+        <div className='wordContainer'>
+            {word.split('').map((letter, index) => (
+                <span key={index} className='letters'>
+                    {letter}
+                </span>
+            ))}
+        </div>
+    );
 }
