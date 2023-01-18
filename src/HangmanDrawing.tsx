d06f311f0fe97470ccbfe6357ad54453777961f6
Adam Lang <adam.lang-1@hotmail.com>
2023-01-18 21:19:17 +0200

    feat: hangmang drawing css complete

--- src/HangmanDrawing.tsx
+++ src/HangmanDrawing.tsx
@@ -1,29 +1,28 @@
-const HEAD = (
-    <div className="head" />
-)
+const HEAD = <div className='head' />;
 
-const BODY = (
-    <div className="body" />
-)
+const BODY = <div className='body' />;
 
-const RIGHT_ARM = (
-    <div className="right-arm" />
-)
+const RIGHT_ARM = <div className='right-arm' />;
 
-const LEFT_ARM = (
-    <div className="left-arm" />
-)
+const LEFT_ARM = <div className='left-arm' />;
 
+const RIGHT_LEG = <div className='right-leg' />;
+
+const LEFT_LEG = <div className='left-leg' />;
 
 export function HangmanDrawing() {
-    return <div className='hangmanDrawerContainer'>
-        {HEAD}
-        {BODY}
-        {RIGHT_ARM}
-        {LEFT_ARM}
-        <div className="dropDownBar" />
-        <div className="topBar" />
-        <div className="verticalBar"/>
-        <div className="bottomBar" />
-    </div>;
+    return (
+        <div className='hangmanDrawerContainer'>
+            {HEAD}
+            {BODY}
+            {RIGHT_ARM}
+            {LEFT_ARM}
+            {RIGHT_LEG}
+            {LEFT_LEG}
+            <div className='dropDownBar' />
+            <div className='topBar' />
+            <div className='verticalBar' />
+            <div className='bottomBar' />
+        </div>
+    );
 }
