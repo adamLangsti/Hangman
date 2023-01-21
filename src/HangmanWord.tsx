958b04e9be3775e3f452cd01e2dd79b25d82bb03
Adam Lang <adam.lang-1@hotmail.com>
2023-01-21 20:45:39 +0200

    feat: looping over letters and applying css style if visible or not

--- src/HangmanWord.tsx
+++ src/HangmanWord.tsx
@@ -2,11 +2,19 @@ import '../src/styles/hangmanword.css';
 
 export function HangmanWord() {
     const word = 'test';
+    const guessedLetters = ['t', "s", "f"];
     return (
         <div className='wordContainer'>
             {word.split('').map((letter, index) => (
                 <span key={index} className='letters'>
-                    {letter}
+                    <span
+                        className={
+                            guessedLetters.includes(letter)
+                                ? 'letter-visible'
+                                : 'letter-invisible'
+                        }>
+                        {letter}
+                    </span>
                 </span>
             ))}
         </div>
