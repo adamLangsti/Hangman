965feeca34a84d86e37ba5350319b9f376b57f31
Adam Lang <adam.lang-1@hotmail.com>
2023-01-27 13:47:59 +0200

    feat: hangman body collected in array

--- src/HangmanDrawing.tsx
+++ src/HangmanDrawing.tsx
@@ -10,15 +10,16 @@ const RIGHT_LEG = <div className='right-leg' />;
 
 const LEFT_LEG = <div className='left-leg' />;
 
-export function HangmanDrawing() {
+const BODY_PARTS = [HEAD, BODY, RIGHT_ARM, LEFT_ARM, RIGHT_LEG, LEFT_LEG];
+
+type HangmanDrawingProps = {
+    numberOfGuesses: number;
+};
+
+export function HangmanDrawing({ numberOfGuesses }: HangmanDrawingProps) {
     return (
         <div className='hangmanDrawerContainer'>
-            {HEAD}
-            {BODY}
-            {RIGHT_ARM}
-            {LEFT_ARM}
-            {RIGHT_LEG}
-            {LEFT_LEG}
+            {BODY_PARTS.slice(0, numberOfGuesses)}
             <div className='dropDownBar' />
             <div className='topBar' />
             <div className='verticalBar' />
