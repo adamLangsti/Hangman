848b72ffd42ea01d123b790a6a79fce272ffc36a
Adam Lang <adam.lang-1@hotmail.com>
2023-01-27 14:24:55 +0200

    feat: reveal word if incorrect guesses, inline styling

--- src/HangmanWord.tsx
+++ src/HangmanWord.tsx
@@ -3,19 +3,29 @@ import '../src/styles/hangmanword.css';
 type HangmanWordProps = {
     guessedLetters: string[];
     wordToGuess: string;
+    reveal?: boolean;
 };
 
-export function HangmanWord({ guessedLetters, wordToGuess }: HangmanWordProps) {
+export function HangmanWord({
+    guessedLetters,
+    wordToGuess,
+    reveal = false,
+}: HangmanWordProps) {
     return (
         <div className='wordContainer'>
             {wordToGuess.split('').map((letter, index) => (
                 <span key={index} className='letters'>
                     <span
-                        className={
-                            guessedLetters.includes(letter)
-                                ? 'letter-visible'
-                                : 'letter-invisible'
-                        }>
+                        style={{
+                            visibility:
+                                guessedLetters.includes(letter) || reveal
+                                    ? 'visible'
+                                    : 'hidden',
+                            color:
+                                !guessedLetters.includes(letter) && reveal
+                                    ? 'red'
+                                    : 'black',
+                        }}>
                         {letter}
                     </span>
                 </span>
