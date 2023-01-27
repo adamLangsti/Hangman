1d38e6714d3e72b0de35d27f24806b13e419915e
Adam Lang <adam.lang-1@hotmail.com>
2023-01-27 13:50:21 +0200

    feat: taking props and rendering css based on boolean value

--- src/Keyboard.tsx
+++ src/Keyboard.tsx
@@ -1,4 +1,6 @@
+import { useState } from 'react';
 import '../src/styles/keyboard.css';
+import styles from './Keyboard.module.css';
 
 const KEYS = [
     'a',
@@ -29,12 +31,34 @@ const KEYS = [
     'z',
 ];
 
-export function Keyboard() {
+type KeyboardProps = {
+    activeLetter: string[];
+    inactiveLetters: string[];
+    addGuessedLetter: (letter: string) => void;
+};
+
+export function Keyboard({
+    activeLetter,
+    inactiveLetters,
+    addGuessedLetter,
+}: KeyboardProps) {
     return (
         <div className='keyboard'>
-            {KEYS.map((key) => (
-                <button className='keyboard-button' key={key}>{key}</button>
-            ))}
+            {KEYS.map((key) => {
+                const isActive = activeLetter.includes(key);
+                const inActive = inactiveLetters.includes(key);
+                return (
+                    <button
+                        onClick={() => addGuessedLetter(key)}
+                        className={`${styles.btn} ${
+                            isActive ? 'btn-active' : ''
+                        } ${inActive ? 'btn-inactive' : ''}`}
+                        disabled={isActive || inActive}
+                        key={key}>
+                        {key}
+                    </button>
+                );
+            })}
         </div>
     );
 }
